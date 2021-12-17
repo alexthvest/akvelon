10a408b816b7c1c975d0fdc7380bdeb64652ebd4
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-17 16:35:14 +0300

    Implement Find the missing letter task

---
+++ codewars/Find the missing letter/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FindMissingLetter;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sequence = new[] { 'O', 'Q', 'R', 'S' };
+        var result = MissingLetterFinder.Find(sequence);
+
+        Console.WriteLine(result);
+    }
+}
