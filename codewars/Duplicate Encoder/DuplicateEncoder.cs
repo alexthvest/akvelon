7902991848d24cacc7189bf089c60cf06597861c
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-17 16:09:46 +0300

    Implement duplicate encoder task

---
+++ codewars/Duplicate Encoder/DuplicateEncoder.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace DuplicateEncoder;
+
+internal static class DuplicateEncoder
+{
+    public static string Encode(string value)
+    {
+        value = value.ToLowerInvariant();
+
+        var builder = new StringBuilder();
+        var dictionary = new Dictionary<char, int>();
+
+        foreach (var @char in value)
+        {
+            if (dictionary.ContainsKey(@char))
+            {
+                dictionary[@char] += 1;
+                continue;
+            }
+
+            dictionary.Add(@char, 1);
+        }
+
+        foreach (var @char in value)
+        {
+            var paren = dictionary[@char] == 1 ? "(" : ")";
+            builder.Append(paren);
+        }
+
+        return builder.ToString();
+    }
+}
