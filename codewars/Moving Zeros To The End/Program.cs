eb204e82b349f31c3987440be09b3c86186d4d55
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-17 17:43:28 +0300

    Implemented Move zeros to the end Codewars task

---
+++ codewars/Moving Zeros To The End/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MovingZerosToTheEnd;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var sequence = new[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 };
+        var result = ZerosMover.MoveToEnd(sequence);
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
---
+++ codewars/Moving Zeros To The End/ZerosMover.cs
@@ -0,0 +1,28 @@
+﻿namespace MovingZerosToTheEnd;
+
+internal static class ZerosMover
+{
+    public static int[] MoveToEnd(int[] sequence)
+    {
+        var result = new List<int>();
+        var zeroCounter = 0;
+
+        foreach (var number in sequence)
+        {
+            if (number == 0)
+            {
+                zeroCounter++;
+                continue;
+            }
+
+            result.Add(number);
+        }
+
+        for (var i = 0; i < zeroCounter; i++)
+        {
+            result.Add(0);
+        }
+
+        return result.ToArray();
+    }
+}
