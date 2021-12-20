cd6240480eb2caead8af1c1295efb220aa0f097e
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-20 16:25:17 +0300

    Implemented Sum of intervals task

---
+++ codewars/Sum of Intervals/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumOfIntervals;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var intervals = new[]
+        {
+            (1, 2), 
+            (6, 10), 
+            (11, 15)
+        };
+
+        var result = IntervalSummer.Sum(intervals);
+        Console.WriteLine(result);
+    }
+}
