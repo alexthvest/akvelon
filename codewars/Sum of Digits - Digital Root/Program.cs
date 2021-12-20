badd8a35706f1d3968e1be871cd79a74b9624bff
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-20 10:59:31 +0300

    Implement digital root task

---
+++ codewars/Sum of Digits - Digital Root/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DigitalRoot;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var value = 493193;
+        var result = RecursiveDigitSummer.Sum(value);
+
+        Console.WriteLine(result);
+    }
+}
