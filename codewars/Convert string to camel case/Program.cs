78d9cdf6bd2318c0647539cee29bb6160316e341
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-20 10:09:38 +0300

    Implement convertion of string to camel case task

---
+++ codewars/Convert string to camel case/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConvertToCamelCase;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var value = "the-stealth-warrior";
+        var result = CamelCaseConverter.Convert(value);
+
+        Console.WriteLine(result);
+    }
+}
