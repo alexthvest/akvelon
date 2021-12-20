31f6222fecb2f81e4ad756a7212985ef1fe12bec
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-20 11:29:57 +0300

    Implement Build Tower Codewars task

---
+++ codewars/Build Tower/TowerBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace BuildTower;
+
+internal static class TowerBuilder
+{
+    public static string[] Build(int floors)
+    {
+        var tower = new string[floors];
+
+        for (var i = 1; i <= floors; i++)
+        {
+            var floor = new string('*', i * 2 - 1);
+            var padding = new string(' ', floors - i);
+
+            tower[i - 1] = padding + floor + padding;
+        }
+
+        return tower;
+    }
+}
