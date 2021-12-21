4a6809ce16bb2d08b24a82b9b1997884432dccd8
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-21 13:12:04 +0300

    Implement 'Product of consecutive Fib numbers'

---
+++ codewars/Product of consecutive Fib numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FibProduct;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var result = FibProductFinder.Find(4895);
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
