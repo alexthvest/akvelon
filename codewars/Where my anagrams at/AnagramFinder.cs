da3a091af92f70acfb232fa0e9fa67d1b790afe7
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-21 13:35:07 +0300

    Implement 'Where my anagrams at?' task

---
+++ codewars/Where my anagrams at/AnagramFinder.cs
@@ -0,0 +1,19 @@
+﻿namespace WhereMyAnagramsAt;
+
+internal static class AnagramFinder
+{
+    public static IEnumerable<string> Find(string word, ICollection<string> options)
+    {
+        var sortedWord = word.OrderBy(c => c);
+
+        foreach (var option in options)
+        {
+            var sortedOption = option.OrderBy(c => c);
+
+            if (sortedWord.SequenceEqual(sortedOption))
+            {
+                yield return option;
+            }
+        }
+    }
+}
