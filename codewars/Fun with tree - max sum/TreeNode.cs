c7f61baedd5a0735fca201b36e63fa5f4f073de5
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-22 13:43:32 +0300

    Implement 'Fun with trees: max sum'

---
+++ codewars/Fun with tree - max sum/TreeNode.cs
@@ -0,0 +1,15 @@
+﻿namespace TreeMaxSum;
+
+internal class TreeNode
+{
+    public TreeNode(int value, TreeNode? left = null, TreeNode? right = null)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+
+    public int Value { get; }
+    public TreeNode? Left { get; }
+    public TreeNode? Right { get; }
+}
