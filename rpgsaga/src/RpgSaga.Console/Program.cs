4fac169319ec9d1dd61d8551cfce39a09869da4f
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-24 12:59:15 +0300

    Add RpgSaga.Core project containing core logic of game

---
+++ rpgsaga/src/RpgSaga.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using RpgSaga.Core;
+
+namespace RpgSaga;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var hero = new Hero("Guitar");
+        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+        Console.WriteLine($"Hello, {hero.Name}!");
+        Console.WriteLine();
+        Console.WriteLine($"AssemblyVersion: {assemblyVersion}");
+    }
+}
---
+++ rpgsaga/src/RpgSaga.Core/Hero.cs
@@ -0,0 +1,11 @@
+﻿namespace RpgSaga.Core;
+
+public class Hero
+{
+    public Hero(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+}
