558b5ef2211243208699171b2908c95ac8c71bc0
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-27 10:36:32 +0300

    Create Game and GameBuilder classes
    Create main classes and configure IHosting with configuration, logging and DI
    services.

---
+++ rpgsaga/src/RpgSaga.Core/Game.cs
@@ -0,0 +1,21 @@
+﻿namespace RpgSaga.Core;
+
+public sealed class Game
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    internal Game(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public static GameBuilder CreateBuilder()
+    {
+        return new GameBuilder();
+    }
+
+    public void Start(byte playerCount)
+    {
+        Console.WriteLine(playerCount);
+    }
+}
---
+++ rpgsaga/src/RpgSaga.Core/GameBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RpgSaga.Core;
+
+public sealed class GameBuilder
+{
+    private readonly ServiceCollection _services = new ();
+
+    public IServiceCollection Services => _services;
+
+    public Game Build()
+    {
+        var serviceProvider = _services.BuildServiceProvider();
+        return new Game(serviceProvider);
+    }
+}
