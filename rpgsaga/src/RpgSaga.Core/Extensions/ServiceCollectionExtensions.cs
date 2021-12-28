978317abf9da8e045031b03741133cf8bfa7dc90
Daniil Golubev <daniil.golubev@akvelon.com>
2021-12-28 13:48:52 +0300

    Create new heroes (Knight, Archer, Mage) and random hero generating logic

--- rpgsaga/src/RpgSaga.Core/Extensions/ServiceCollectionExtensions.cs
+++ rpgsaga/src/RpgSaga.Core/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using RpgSaga.Core.Abstractions;
 using RpgSaga.Core.Logic;
 using RpgSaga.Core.Managment;
+using RpgSaga.Core.Storages;
 
 namespace RpgSaga.Core.Extensions;
 
@@ -9,10 +10,14 @@ internal static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddRpgSagaCore(this IServiceCollection services)
     {
-        services.AddSingleton<IGameLoop, GameLoop>();
         services.AddSingleton<IRoundHandler, RoundHandler>();
         services.AddSingleton<IDuelHandler, DuelHandler>();
+
+        services.AddSingleton<IHeroStorage, HeroStorage>();
+
         services.AddSingleton<IRoundPairGenerator, RoundPairGenerator>();
+        services.AddSingleton<IRandomNameGenerator, RandomNameGenerator>();
+        services.AddSingleton<IRandomHeroGenerator, RandomHeroGenerator>();
 
         return services;
     }
---
+++ rpgsaga/src/RpgSaga.DLCs/Rogue/Heroes/Mage.cs
@@ -0,0 +1,16 @@
+﻿using RpgSaga.Core.Models;
+
+namespace RpgSaga.DLCs.Rogue.Heroes;
+
+public sealed class Mage : Hero
+{
+    public Mage(string firstName, string lastName, double health, double attack) 
+        : base(firstName, lastName, health, attack)
+    {
+    }
+
+    public static Mage Create(string firstName, string lastName, double health, double attack)
+    {
+        return new Mage(firstName, lastName, health, attack);
+    }
+}
