fc2116da922d9116100485ee984f1057afb14765
Daniil Golubev <daniil.golubev@akvelon.com>
2022-01-17 13:03:09 +0300

    Fix TestData to follow new product entity schema

--- onlineshop/server/tests/OnlineShop.Web.MockFactory.Tests/Data/TestProducts.cs
+++ onlineshop/server/tests/OnlineShop.Web.MockFactory.Tests/Data/TestProducts.cs
@@ -4,11 +4,11 @@ namespace WebAPI.MockFactory.Tests.Data;
 
 public static class TestProducts
 {
-    public static Product ProductA => new () { Name = "ProductA" };
+    public static Product ProductA => new () { Name = "ProductA", Description = "ProductA Desc", Price = 0 };
 
-    public static Product ProductB => new () { Name = "ProductB" };
+    public static Product ProductB => new () { Name = "ProductB", Description = "ProductB Desc", Price = 0 };
 
-    public static Product ProductC => new () { Name = "ProductC" };
+    public static Product ProductC => new () { Name = "ProductC", Description = "ProductC Desc", Price = 0 };
 
     public static List<Product> AllProducts => new () { ProductA, ProductB, ProductC };
 }
