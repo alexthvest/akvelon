47302e0d78f8f18fcdbbf5331aee5027d8bb2be1
Daniil Golubev <daniil.golubev@akvelon.com>
2022-01-19 09:50:45 +0300

    Add AutoMapper

---
+++ onlineshop/server/src/OnlineShop.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace OnlineShop.Application.Common.Mappings;
+
+internal class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        var types = assembly.GetExportedTypes()
+            .Where(t => t
+                .GetInterfaces()
+                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+            .ToArray();
+
+        foreach (var type in types)
+        {
+            var instance = Activator.CreateInstance(type);
+
+            var methodInfo = type.GetMethod("Map") 
+                ?? type.GetInterface("IMapFrom`1")?.GetMethod("Map");
+
+            methodInfo?.Invoke(instance, new object[] { this });
+        }
+    }
+}
